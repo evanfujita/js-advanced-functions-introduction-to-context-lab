5db574cf11cf73a041ecc64e858ecc0e3757b9bf
Evan <evanfujita@gmail.com>
2021-02-14 16:29:33 -0600

    commit tree 2c795090823d78268a7dce0da641bf3c54d75e79

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+
+
+const createEmployeeRecord = function(array){
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+
+}
+
+const createEmployeeRecords = function(array){
+    return array.map(employee => {
+        // let createTimeInEvent = function(employee){
+        //     employee.timeInEvents
+        // }
+        
+        return createEmployeeRecord(employee)
+
+    })
+
+}
+
+
+const findEmployeeByFirstName = function(array, name){
+    return array.find(element =>{
+        return element.firstName === name
+    })
+
+}
